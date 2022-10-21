ab5a2fa0a880ab5cc305f613337edad33ca0240e
Taranchuk <nfhfyxer@gmail.com>
2022-10-21 20:42:41 +0300

    1.4 fix

--- 1.4/Source/VanillaPsycastsExpanded/Paths/Empath/AbilityExtension_MindWipe.cs
+++ 1.4/Source/VanillaPsycastsExpanded/Paths/Empath/AbilityExtension_MindWipe.cs
@@ -1,35 +1,35 @@
-﻿namespace VanillaPsycastsExpanded
-{
-    using RimWorld;
-    using RimWorld.Planet;
-    using System.Collections.Generic;
-    using System.Security.Cryptography;
-    using Verse;
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using Verse;
 using VFECore;
-    using VFECore.Abilities;
-    using Ability = VFECore.Abilities.Ability;
+using VFECore.Abilities;
+using Ability = VFECore.Abilities.Ability;
+
+namespace VanillaPsycastsExpanded
+{
     public class AbilityExtension_MindWipe : AbilityExtension_AbilityMod
-	{
+    {
         public override void Cast(GlobalTargetInfo[] targets, Ability ability)
         {
             base.Cast(targets, ability);
-            foreach (var target in targets)
+            foreach (GlobalTargetInfo target in targets)
             {
-                var pawn = target.Thing as Pawn;
+                Pawn pawn = target.Thing as Pawn;
                 if (pawn.Faction != ability.pawn.Faction)
                 {
                     pawn.SetFaction(ability.pawn.Faction);
                 }
                 pawn.needs.mood.thoughts.memories.Memories.Clear();
                 pawn.relations.ClearAllRelations();
-                var passions = new Dictionary<SkillDef, Passion>();
-                foreach (var skillRecord in pawn.skills.skills)
+                Dictionary<SkillDef, Passion> passions = new();
+                foreach (SkillRecord skillRecord in pawn.skills.skills)
                 {
                     passions[skillRecord.def] = skillRecord.passion;
                 }
                 pawn.skills = new Pawn_SkillTracker(pawn);
-                NonPublicMethods.GenerateSkills(pawn);
-                foreach (var kvp in passions)
+                NonPublicMethods.GenerateSkills(pawn, new PawnGenerationRequest(pawn.kindDef, pawn.Faction));
+                foreach (KeyValuePair<SkillDef, Passion> kvp in passions)
                 {
                     pawn.skills.GetSkill(kvp.Key).passion = kvp.Value;
                 }
