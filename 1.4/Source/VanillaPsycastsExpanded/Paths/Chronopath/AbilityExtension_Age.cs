3a35d6e8452d4833f717950b520e7d839ba8fdd7
Joshua <legodude17@users.noreply.github.com>
2022-10-24 15:10:13 -0700

    Fix stats and aging

--- 1.4/Source/VanillaPsycastsExpanded/Paths/Chronopath/AbilityExtension_Age.cs
+++ 1.4/Source/VanillaPsycastsExpanded/Paths/Chronopath/AbilityExtension_Age.cs
@@ -1,32 +1,38 @@
-﻿namespace VanillaPsycastsExpanded.Chronopath;
-
-using System.Collections.Generic;
-using System.Linq;
+﻿using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
+using UnityEngine;
 using Verse;
 using VFECore.Abilities;
 using Ability = VFECore.Abilities.Ability;
 
+namespace VanillaPsycastsExpanded.Chronopath;
+
 public class AbilityExtension_Age : AbilityExtension_AbilityMod
 {
-    public float? targetYears = null;
+    private static readonly AccessTools.FieldRef<Pawn_AgeTracker, float> progressToNextBiologicalTick =
+        AccessTools.FieldRefAccess<Pawn_AgeTracker, float>("progressToNextBiologicalTick");
+
+    private static readonly AccessTools.FieldRef<Pawn_AgeTracker, long> ageBiologicalTicksInt =
+        AccessTools.FieldRefAccess<Pawn_AgeTracker, long>("ageBiologicalTicksInt");
+
     public float? casterYears = null;
+    public float? targetYears = null;
 
     public override void Cast(GlobalTargetInfo[] targets, Ability ability)
     {
         base.Cast(targets, ability);
-        if (this.casterYears.HasValue) Age(ability.pawn, this.casterYears.Value);
-        if (!this.targetYears.HasValue) return;
-        foreach (GlobalTargetInfo target in targets)
+        if (casterYears.HasValue) Age(ability.pawn, casterYears.Value);
+        if (!targetYears.HasValue) return;
+        foreach (var target in targets)
             if (target.Thing is Pawn pawn)
-                Age(pawn, this.targetYears.Value);
+                Age(pawn, targetYears.Value);
     }
 
     public override bool CanApplyOn(LocalTargetInfo target, Ability ability, bool throwMessages = false)
     {
         if (!base.CanApplyOn(target, ability, throwMessages)) return false;
-        if (!this.targetYears.HasValue) return true;
+        if (!targetYears.HasValue) return true;
         if (target.Thing is not Pawn pawn) return false;
         if (!pawn.RaceProps.IsFlesh) return false;
         if (!pawn.RaceProps.Humanlike) return false;
@@ -35,15 +41,23 @@ public class AbilityExtension_Age : AbilityExtension_AbilityMod
 
     public static void Age(Pawn pawn, float years)
     {
-        if (years < 0 && pawn.ageTracker.AgeBiologicalYears <= 16) return;
+        if (years < 0)
+        {
+            var progress = -years * GenDate.TicksPerYear * pawn.ageTracker.BiologicalTicksPerTick;
+            long ticks = Mathf.FloorToInt(progress);
+            progress -= ticks;
+            progressToNextBiologicalTick(pawn.ageTracker) -= progress;
+            ageBiologicalTicksInt(pawn.ageTracker) -= ticks;
+        }
+
         pawn.ageTracker.AgeTickMothballed((int)(years * GenDate.TicksPerYear));
         if (years < 0)
         {
-            List<HediffGiverSetDef> giverSets = pawn.def.race.hediffGiverSets;
+            var giverSets = pawn.def.race.hediffGiverSets;
             if (giverSets == null) return;
-            float lifeFraction = pawn.ageTracker.AgeBiologicalYears / pawn.def.race.lifeExpectancy;
-            foreach (HediffGiverSetDef giverSet in giverSets)
-            foreach (HediffGiver giver in giverSet.hediffGivers)
+            var lifeFraction = pawn.ageTracker.AgeBiologicalYears / pawn.def.race.lifeExpectancy;
+            foreach (var giverSet in giverSets)
+            foreach (var giver in giverSet.hediffGivers)
                 if (giver is HediffGiver_Birthday giverBirthday)
                     if (giverBirthday.ageFractionChanceCurve.Evaluate(lifeFraction) <= 0f)
                     {
@@ -52,13 +66,10 @@ public class AbilityExtension_Age : AbilityExtension_AbilityMod
                     }
         }
 
-        if (pawn.ageTracker.AgeBiologicalYears < 16)
-            pawn.ageTracker.AgeTickMothballed(GenDate.TicksPerYear * 16 - (int)pawn.ageTracker.AgeBiologicalTicks);
-
         if (pawn.ageTracker.AgeBiologicalYears > pawn.def.race.lifeExpectancy * 1.1f)
         {
-            BodyPartRecord part   = pawn.RaceProps.body.AllParts.FirstOrDefault(p => p.def == BodyPartDefOf.Heart);
-            Hediff         hediff = HediffMaker.MakeHediff(VPE_DefOf.HeartAttack, pawn, part);
+            var part = pawn.RaceProps.body.AllParts.FirstOrDefault(p => p.def == BodyPartDefOf.Heart);
+            var hediff = HediffMaker.MakeHediff(VPE_DefOf.HeartAttack, pawn, part);
             hediff.Severity = 1.1f;
             pawn.health.AddHediff(hediff, part);
         }
