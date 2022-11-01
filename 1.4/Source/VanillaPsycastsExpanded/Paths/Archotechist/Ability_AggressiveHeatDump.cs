f1b5a48301185670dc2652727e9a91f8bb700397
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-10-31 19:25:15 -0600

    Aggressive Heat Dump enhancements
    - Radius now displays properly
    - Description lied about scaling when it wasn't present, now scales (defined in XML)
    - Power value moved to XML
    - Radius value moved to XML

--- 1.4/Source/VanillaPsycastsExpanded/Paths/Archotechist/Ability_AggressiveHeatDump.cs
+++ 1.4/Source/VanillaPsycastsExpanded/Paths/Archotechist/Ability_AggressiveHeatDump.cs
@@ -12,16 +12,21 @@
 
     public class Ability_AggressiveHeatDump : Ability
     {
+        public override float GetRadiusForPawn() =>
+            Mathf.Ceil(Mathf.Min(this.pawn.psychicEntropy.EntropyValue / 20f, 9f * base.GetRadiusForPawn()));
+
+        public override float GetPowerForPawn() => DamageDefOf.Flame.defaultDamage *
+                                                   this.pawn.psychicEntropy.EntropyValue * base.GetPowerForPawn();
+
         public override void Cast(params GlobalTargetInfo[] targets)
         {
             base.Cast(targets);
-            var damageMultiplier = this.pawn.psychicEntropy.EntropyValue / 100f;
-            var explosionRadius = Mathf.Min(Mathf.Ceil(this.pawn.psychicEntropy.EntropyValue / 20f), 9f);
+            var explosionRadius = this.GetRadiusForPawn();
             this.pawn.psychicEntropy.RemoveAllEntropy();
             MakeStaticFleck(targets[0].Cell, targets[0].Thing.Map, FleckDefOf.PsycastAreaEffect, explosionRadius, 0);
             MakeStaticFleck(targets[0].Cell, targets[0].Thing.Map, VPE_DefOf.VPE_AggresiveHeatDump, explosionRadius, 0);
             GenExplosion.DoExplosion(targets[0].Cell, pawn.Map, explosionRadius, DamageDefOf.Flame, pawn,
-                (int)(DamageDefOf.Flame.defaultDamage * damageMultiplier), ignoredThings: new List<Thing> { pawn });
+                (int)this.GetPowerForPawn(), ignoredThings: new List<Thing> { pawn });
         }
     }
 }
