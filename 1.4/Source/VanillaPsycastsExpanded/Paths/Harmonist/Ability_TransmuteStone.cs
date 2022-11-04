5b8b4d570266f80cc011173a0ed78da13c5778b6
Joshua <legodude17@users.noreply.github.com>
2022-11-04 12:21:33 -0700

    Use better method in transmute stone

--- 1.4/Source/VanillaPsycastsExpanded/Paths/Harmonist/Ability_TransmuteStone.cs
+++ 1.4/Source/VanillaPsycastsExpanded/Paths/Harmonist/Ability_TransmuteStone.cs
@@ -1,10 +1,10 @@
-﻿namespace VanillaPsycastsExpanded.Harmonist;
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using HarmonyLib;
 using RimWorld.Planet;
 using Verse;
 using VFECore.Abilities;
+
+namespace VanillaPsycastsExpanded.Harmonist;
 
 public class Ability_TransmuteStone : Ability
 {
@@ -16,61 +16,65 @@ public class Ability_TransmuteStone : Ability
     public override void Cast(params GlobalTargetInfo[] targets)
     {
         base.Cast(targets);
-        foreach (GlobalTargetInfo target in targets)
+        foreach (var target in targets)
         {
-            Map map = target.Map;
+            var map = target.Map;
             Find.World.NaturalRockTypesIn(map.Tile); // Force the game to generate the rocks list we are querying
-            List<ThingDef> naturalRockDefs = allNaturalRockDefs(Find.World);
-            ThingDef       chosenRock      = naturalRockDefs.RandomElement();
+            var naturalRockDefs = allNaturalRockDefs(Find.World);
+            var chosenRock = naturalRockDefs.RandomElement();
 
             TerrainDef NewTerrain(TerrainDef terrain)
             {
-                string name = terrain.defName;
-                foreach (ThingDef rockDef in naturalRockDefs.Except(chosenRock))
+                var name = terrain.defName;
+                foreach (var rockDef in naturalRockDefs.Except(chosenRock))
                     if (name.StartsWith(rockDef.defName))
                         name = name.Replace(rockDef.defName, chosenRock.defName);
                 return TerrainDef.Named(name);
             }
 
-            void ClearCache(Thing thing)
+            void Replace(Thing thing, ThingDef def = null, ThingDef stuff = null)
             {
-                graphicInt(thing) = null;
-                thing.DirtyMapMesh(map);
+                var owner = thing.holdingOwner;
+                var cell = thing.Position;
+                var rot = thing.Rotation;
+                def ??= thing.def;
+                stuff ??= thing.Stuff;
+                var newThing = ThingMaker.MakeThing(def, stuff);
+                var des = map.designationManager.AllDesignationsOn(thing).ListFullCopy();
+                thing.Destroy();
+                if (cell.IsValid)
+                    GenSpawn.Spawn(newThing, cell, map, rot);
+                else if (owner != null)
+                {
+                    if (!owner.TryAdd(newThing)) Log.Error($"[VPE] Failed to add {newThing} to {owner}");
+                }
+                else
+                {
+                    Log.Warning($"[VPE] Attempting to replace unspawned and unheld thing {thing}");
+                    return;
+                }
+
+                foreach (var designation in des) map.designationManager.AddDesignation(new Designation(newThing, designation.def));
             }
 
-            foreach (IntVec3 cell in GenRadial.RadialCellsAround(target.Cell, this.GetRadiusForPawn(), true))
+            foreach (var cell in GenRadial.RadialCellsAround(target.Cell, GetRadiusForPawn(), true))
             {
-                foreach (Thing thing in cell.GetThingList(map))
+                foreach (var thing in cell.GetThingList(map).ListFullCopy())
                     if (thing.def.IsNonResourceNaturalRock)
-                    {
-                        thing.def = chosenRock;
-                        ClearCache(thing);
-                    }
+                        Replace(thing, chosenRock);
                     else
-                        foreach (ThingDef rockDef in naturalRockDefs)
+                        foreach (var rockDef in naturalRockDefs)
                             if (rockDef.building.mineableThing == thing.def)
-                            {
-                                thing.def = chosenRock.building.mineableThing;
-                                ClearCache(thing);
-                            }
+                                Replace(thing, chosenRock.building.mineableThing);
                             else if (rockDef.building.smoothedThing == thing.def)
-                            {
-                                thing.def = chosenRock.building.smoothedThing;
-                                ClearCache(thing);
-                            }
+                                Replace(thing, chosenRock.building.smoothedThing);
                             else if (rockDef.building.mineableThing.butcherProducts[0].thingDef == thing.def)
-                            {
-                                thing.def = chosenRock.building.mineableThing.butcherProducts[0].thingDef;
-                                ClearCache(thing);
-                            }
+                                Replace(thing, chosenRock.building.mineableThing.butcherProducts[0].thingDef);
                             else if (thing.Stuff != null && thing.Stuff == rockDef.building.mineableThing.butcherProducts[0].thingDef)
-                            {
-                                thing.SetStuffDirect(chosenRock.building.mineableThing.butcherProducts[0].thingDef);
-                                ClearCache(thing);
-                            }
+                                Replace(thing, stuff: chosenRock.building.mineableThing.butcherProducts[0].thingDef);
 
-                TerrainGrid grid    = map.terrainGrid;
-                TerrainDef  terrain = grid.TerrainAt(cell);
+                var grid = map.terrainGrid;
+                var terrain = grid.TerrainAt(cell);
                 grid.SetTerrain(cell, NewTerrain(terrain));
                 terrain = grid.UnderTerrainAt(cell);
                 if (terrain != null) grid.SetUnderTerrain(cell, NewTerrain(terrain));
