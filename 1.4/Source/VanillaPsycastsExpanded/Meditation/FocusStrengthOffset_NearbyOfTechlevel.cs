b9796dbda6a762152622dba6430fb3b370a9a467
Joshua <legodude17@users.noreply.github.com>
2023-01-07 14:41:54 -0800

    Fix NRE on unspawned foci

--- 1.4/Source/VanillaPsycastsExpanded/Meditation/FocusStrengthOffset_NearbyOfTechlevel.cs
+++ 1.4/Source/VanillaPsycastsExpanded/Meditation/FocusStrengthOffset_NearbyOfTechlevel.cs
@@ -1,30 +1,30 @@
-﻿namespace VanillaPsycastsExpanded;
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using RimWorld;
 using UnityEngine;
 using Verse;
 
+namespace VanillaPsycastsExpanded;
+
 public class FocusStrengthOffset_NearbyOfTechlevel : FocusStrengthOffset
 {
+    public float radius = 10f;
     public TechLevel techLevel;
-    public float     radius = 10f;
 
     public override float GetOffset(Thing parent, Pawn user = null)
     {
-        int   count;
+        int count;
         float x;
-        if (parent.Map == null)
+        if (parent.MapHeld is { } map)
         {
-            count = 1;
-            x     = parent.MarketValue * parent.stackCount;
+            var things = GetThings(parent.Position, map);
+            count = Mathf.Clamp(things.Count, 1, 10);
+            x = Mathf.Clamp(things.Sum(t => t.MarketValue * t.stackCount), 1, 5000);
         }
         else
         {
-            List<Thing> things = this.GetThings(parent.Position, parent.Map);
-            count = Mathf.Clamp(things.Count,                                  1, 10);
-            x     = Mathf.Clamp(things.Sum(t => t.MarketValue * t.stackCount), 1, 5000);
+            count = 1;
+            x = parent.MarketValue * parent.stackCount;
         }
 
         return count / 5.55f * x / 10000f;
@@ -32,20 +32,21 @@ public class FocusStrengthOffset_NearbyOfTechlevel : FocusStrengthOffset
 
     public override string GetExplanation(Thing parent)
     {
-        int num = parent.Map == null ? 1 : this.GetThings(parent.Position, parent.Map).Count;
-        return "VPE.ThingsOfLevel".Translate(num, this.techLevel.ToString()) + ": " + this.GetOffset(parent).ToStringWithSign("0%");
+        var num = parent.MapHeld is { } map ? GetThings(parent.Position, map).Count : 1;
+        return "VPE.ThingsOfLevel".Translate(num, techLevel.ToString()) + ": " + GetOffset(parent).ToStringWithSign("0%");
     }
 
     public override void PostDrawExtraSelectionOverlays(Thing parent, Pawn user = null)
     {
         base.PostDrawExtraSelectionOverlays(parent, user);
-        GenDraw.DrawRadiusRing(parent.Position, this.radius, PlaceWorker_MeditationOffsetBuildingsNear.RingColor);
-        foreach (Thing thing in this.GetThings(parent.Position, parent.Map))
-            GenDraw.DrawLineBetween(parent.TrueCenter(), thing.TrueCenter(), SimpleColor.Green);
+        GenDraw.DrawRadiusRing(parent.Position, radius, PlaceWorker_MeditationOffsetBuildingsNear.RingColor);
+        if (parent.MapHeld is { } map)
+            foreach (var thing in GetThings(parent.Position, map))
+                GenDraw.DrawLineBetween(parent.TrueCenter(), thing.TrueCenter(), SimpleColor.Green);
     }
 
     protected virtual List<Thing> GetThings(IntVec3 cell, Map map)
     {
-        return GenRadialCached.RadialDistinctThingsAround(cell, map, this.radius, true).Where(t => t.def.techLevel == this.techLevel).Take(10).ToList();
+        return GenRadialCached.RadialDistinctThingsAround(cell, map, radius, true).Where(t => t.def.techLevel == techLevel).Take(10).ToList();
     }
 }
