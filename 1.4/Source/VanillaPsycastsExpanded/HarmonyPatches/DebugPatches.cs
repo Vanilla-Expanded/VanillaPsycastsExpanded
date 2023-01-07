05f549e231d5ebb501bd6560d0be4a97d2471292
Joshua <legodude17@users.noreply.github.com>
2023-01-07 14:31:22 -0800

    Better fix for max level and remove logs

--- 1.4/Source/VanillaPsycastsExpanded/HarmonyPatches/DebugPatches.cs
+++ 1.4/Source/VanillaPsycastsExpanded/HarmonyPatches/DebugPatches.cs
@@ -6,41 +6,6 @@ using Verse;
 
 namespace VanillaPsycastsExpanded;
 
-//[HarmonyPatch(typeof(RitualRoleAnimaLinker), nameof(RitualRoleAnimaLinker.AppliesToPawn))]
-//public static class AppliesToPawnDebug
-//{
-//    [HarmonyPrefix]
-//    public static void Prefix(ref bool skipReason, Pawn p)
-//    {
-//        skipReason = false;
-//        Log.Message($"max={AccessTools.Method(typeof(PawnUtility), nameof(PawnUtility.GetMaxPsylinkLevel)).Invoke(null, new object[] { p })}");
-//    }
-//
-//    [HarmonyPostfix]
-//    public static void Postfix(Pawn p, bool __result, string reason)
-//    {
-//        Log.Message($"{p} can link: {__result} because {reason}"
-//                  + $"\nnatural={MeditationFocusDefOf.Natural.CanPawnUse(p)}, level={p.GetPsylinkLevel()}, max={p.GetMaxPsylinkLevel()}"
-//                  + $"\nCheck: {!MeditationFocusDefOf.Natural.CanPawnUse(p) || p.GetPsylinkLevel() >= p.GetMaxPsylinkLevel()}");
-//    }
-//
-//    [HarmonyTranspiler]
-//    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
-//    {
-//        var info = AccessTools.Method(typeof(PawnUtility), nameof(PawnUtility.GetMaxPsylinkLevel));
-//        foreach (var instruction in instructions.AddLogs(original, new List<MethodInfo>
-//                 {
-//                     AccessTools.Method(typeof(MeditationFocusDef), nameof(MeditationFocusDef.CanPawnUse)),
-//                     AccessTools.Method(typeof(PawnUtility), nameof(PawnUtility.GetPsylinkLevel)),
-//                     info
-//                 }))
-//            yield return instruction;
-//    }
-//}
-
-[HarmonyPatch]
-public static class MiscDebugPatches { }
-
 public static class DebugHelpers
 {
     public static IEnumerable<CodeInstruction> AddLogs(this IEnumerable<CodeInstruction> instructions, MethodBase original, List<MethodInfo> methods)
