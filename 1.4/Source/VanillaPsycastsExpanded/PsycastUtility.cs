c372afb74ac3c800ce16f17a1749dbdaea444682
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2023-04-15 18:08:56 -0700

    add missing startup attribute

--- 1.4/Source/VanillaPsycastsExpanded/PsycastUtility.cs
+++ 1.4/Source/VanillaPsycastsExpanded/PsycastUtility.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Verse;
 
+[StaticConstructorOnStartup]
 public static class PsycastUtility
 {
     public static readonly HashSet<ThingDef> eltexThings;
