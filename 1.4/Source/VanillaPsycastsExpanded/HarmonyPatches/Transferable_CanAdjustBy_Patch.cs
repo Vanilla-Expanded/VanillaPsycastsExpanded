b5bebae0d7d513c28539a2a8910dd9cfe7e6ca3b
Ryan McKenzie <ryan__mckenzie@hotmail.com>
2023-04-15 23:00:57 -0700

    Improve trade menu performance (#5)
    * change the order in which conditions are checked so we're evaluating conditions from least to most computationally expensive
    
    * cache things made of eltex to improve performance in the trade menu
    
    * split eltex detection into utility class
    
    * refactor if chain
    
    * add missing startup attribute
    
    * make `eltexThings` private

--- 1.4/Source/VanillaPsycastsExpanded/HarmonyPatches/Transferable_CanAdjustBy_Patch.cs
+++ 1.4/Source/VanillaPsycastsExpanded/HarmonyPatches/Transferable_CanAdjustBy_Patch.cs
@@ -10,10 +10,11 @@
 	public static class Transferable_CanAdjustBy_Patch
     {
         public static Transferable curTransferable;
+
 		public static void Postfix(Transferable __instance)
 		{
-            if (curTransferable != __instance && Find.WindowStack.IsOpen<Dialog_Trade>() && TradeSession.trader != null 
-                && __instance.ThingDef.IsEltexOrHasEltexMaterial() &&  TradeSession.trader.Faction != Faction.OfEmpire && __instance.CountToTransferToDestination > 0)
+            if (curTransferable != __instance && Find.WindowStack.IsOpen<Dialog_Trade>() && __instance.CountToTransferToDestination > 0 && TradeSession.trader != null
+                && TradeSession.trader.Faction != Faction.OfEmpire && __instance.ThingDef.IsEltexOrHasEltexMaterial())
             {
                 curTransferable = __instance;
                 if (TradeSession.giftMode)
@@ -26,31 +27,5 @@
                 }
             }
 		}
-
-        public static bool IsEltexOrHasEltexMaterial(this ThingDef def)
-        {
-            if (def != null)
-            {
-                if (def == VPE_DefOf.VPE_Eltex)
-                {
-                    return true;
-                }
-                else if (def.costList != null && def.costList.Any(x => x.thingDef == VPE_DefOf.VPE_Eltex))
-                {
-                    return true;
-                }
-                else
-                {
-                    foreach (var recipe in DefDatabase<RecipeDef>.AllDefs)
-                    {
-                        if (recipe.ProducedThingDef == def && recipe.ingredients.Any(x => x.IsFixedIngredient && x.FixedIngredient == VPE_DefOf.VPE_Eltex))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
     }
 }
--- 1.4/Source/VanillaPsycastsExpanded/PsycastUtility.cs
+++ 1.4/Source/VanillaPsycastsExpanded/PsycastUtility.cs
@@ -1,10 +1,31 @@
 ﻿namespace VanillaPsycastsExpanded;
 
 using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
+[StaticConstructorOnStartup]
 public static class PsycastUtility
 {
+    private static readonly HashSet<ThingDef> eltexThings;
+
+    static PsycastUtility()
+    {
+        eltexThings = DefDatabase<RecipeDef>.AllDefs
+            .Where(recipe => recipe.ingredients.Any(x => x.IsFixedIngredient && x.FixedIngredient == VPE_DefOf.VPE_Eltex))
+            .Select(recipe => recipe.ProducedThingDef)
+            .ToHashSet();
+    }
+
+    public static bool IsEltexOrHasEltexMaterial(this ThingDef def)
+    {
+        return def != null &&
+            (def == VPE_DefOf.VPE_Eltex ||
+            (def.costList != null && def.costList.Any(x => x.thingDef == VPE_DefOf.VPE_Eltex)) ||
+            eltexThings.Contains(def));
+    }
+
     public static Hediff_PsycastAbilities Psycasts(this Pawn pawn) =>
         (Hediff_PsycastAbilities)pawn.health.hediffSet.GetFirstHediffOfDef(VPE_DefOf.VPE_PsycastAbilityImplant);
 
