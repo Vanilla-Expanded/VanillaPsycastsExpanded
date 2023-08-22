a4bd4aecd7b9ec833004d928c7972261a544cfc5
Taranchuk <nfhfyxer@gmail.com>
2023-08-22 14:14:50 +0300

    ensure sacrificed finger hediff stay even after part removal and restoring

---
+++ 1.4/Source/VanillaPsycastsExpanded/HarmonyPatches/Pawn_HealthTracker_RemoveHediff_Patch.cs
@@ -0,0 +1,18 @@
+﻿namespace VanillaPsycastsExpanded
+{
+    using HarmonyLib;
+    using Verse;
+
+    [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.RemoveHediff))]
+    public static class Pawn_HealthTracker_RemoveHediff_Patch
+    {
+        public static bool Prefix(Pawn_HealthTracker __instance, Hediff hediff)
+        {
+            if (hediff.def == VPE_DefOf.VPE_Sacrificed)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
