197bdb228de6bed7519c039a3a94c8c1aa801ba8
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-04-02 09:08:13 +0200

    Fixed construct color

--- 1.5/Source/VanillaPsycastsExpanded/Comps/CompSetStoneColour.cs
+++ 1.5/Source/VanillaPsycastsExpanded/Comps/CompSetStoneColour.cs
@@ -8,39 +8,23 @@
     {
         private ThingDef                      rockDef;
         public  CompProperties_SetStoneColour Props => (CompProperties_SetStoneColour) this.props;
+        public Color color;
 
         public ThingDef KilledLeave => this.rockDef;
 
         public void SetStoneColour(ThingDef thingDef)
         {
             this.rockDef = thingDef;
-            LongEventHandler.ExecuteWhenFinished(this.Apply);
-        }
-
-        private void Apply()
-        {
-            if (this.parent is not Pawn pawn) return;
-            PawnRenderer renderer = pawn.Drawer.renderer;
-            Color        color    = this.rockDef.graphic.data.color;
-            GraphicData  data     = new();
-            data.CopyFrom(pawn.ageTracker.CurKindLifeStage.bodyGraphicData);
-            data.color    = color;
-            data.colorTwo = color;
-            if (!renderer.BodyGraphic) renderer.graphics.ResolveAllGraphics();
-            renderer.graphics.nakedGraphic = data.Graphic;
-            renderer.graphics.ClearCache();
-        }
-
-        public override void PostSpawnSetup(bool respawningAfterLoad)
-        {
-            base.PostSpawnSetup(respawningAfterLoad);
-            if (respawningAfterLoad) LongEventHandler.ExecuteWhenFinished(this.Apply);
+            color = this.rockDef.graphic.data.color;
+            Pawn pawn = this.parent as Pawn;
+            pawn.Drawer.renderer.SetAllGraphicsDirty();
         }
 
         public override void PostExposeData()
         {
             base.PostExposeData();
             Scribe_Defs.Look(ref this.rockDef, nameof(this.rockDef));
+            Scribe_Values.Look(ref color, nameof(this.color));
         }
     }
 }
---
+++ 1.5/Source/VanillaPsycastsExpanded/PawnRenderNodes/PawnRenderNode_RockConstruct.cs
@@ -0,0 +1,24 @@
+﻿
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace VanillaPsycastsExpanded
+{
+    public class PawnRenderNode_RockConstruct : PawnRenderNode_AnimalPart
+    {
+        public PawnRenderNode_RockConstruct(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+            : base(pawn, props, tree)
+        {
+        }
+
+        public override Graphic GraphicFor(Pawn pawn)
+        {
+            if (pawn.TryGetComp(out CompSetStoneColour comp))
+            {
+                Graphic graphic = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.Graphic;
+                return GraphicDatabase.Get<Graphic_Multi>(graphic.path, ShaderDatabase.Cutout, graphic.drawSize, comp.color);
+            }
+            return base.GraphicFor(pawn);
+        }
+    }
+}
