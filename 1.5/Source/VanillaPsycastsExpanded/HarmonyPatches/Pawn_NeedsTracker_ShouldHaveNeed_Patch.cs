ed5a6c63905ba1904f54a84eb54260d5d71e2a32
Taranchuk <nfhfyxer@gmail.com>
2024-08-09 20:30:54 +0300

    fixed pawns failing to generate

--- 1.5/Source/VanillaPsycastsExpanded/HarmonyPatches/Pawn_NeedsTracker_ShouldHaveNeed_Patch.cs
+++ 1.5/Source/VanillaPsycastsExpanded/HarmonyPatches/Pawn_NeedsTracker_ShouldHaveNeed_Patch.cs
@@ -9,7 +9,14 @@ public class Pawn_NeedsTracker_ShouldHaveNeed_Patch
 {
     private static bool Prefix(NeedDef nd, Pawn ___pawn)
     {
-        if ((nd == NeedDefOf.Rest || nd == VPE_DefOf.Joy) && (___pawn.story?.traits?.HasTrait(VPE_DefOf.VPE_Thrall) ?? false)) return false;
+        try
+        {
+            if ((nd == NeedDefOf.Rest || nd == VPE_DefOf.Joy) && (___pawn.story?.traits?.HasTrait(VPE_DefOf.VPE_Thrall) ?? false)) return false;
+        }
+        catch
+        {
+
+        }
         return true;
     }
 }
